4f34861577e058cf786d5745fdcde36b5787676f
Jamshidbek Odiljonov <88502697+Jamsheedbek@users.noreply.github.com>
2022-05-29 17:28:14 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const TOKEN = process.env.TOKEN;
+const express = require("express");
+const app = express();
+
+const bot = new TelegramBot(TOKEN, {
+    polling: true,
+});
+
+// bot.onText(/\/start/, (msg) => {
+//     bot.sendMessage(
+//         '@Shb_TheBestTeam',
+//         "Salom " + msg.from.first_name + " botimizga xush kelibsiz"
+//     );
+// });
+
+bot.on("message", (msg) => {
+    if (msg.from.id == 981288955 && msg.chat.id != -1001268783396) {
+        bot.sendMessage(-1001268783396, msg.text);
+        console.log(msg);
+    }
+});
+
+app.listen(process.env.PORT || 8080, console.log(8080));
