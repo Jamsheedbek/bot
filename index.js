ae5f9718e3d8e7e937849b4061e02987cad00f2d
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-01 14:18:55 +0500

    add try catch

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = process.env.TOKEN;
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 const bot = new TelegramBot(TOKEN, {
     polling: true,
 });
 
 app.get('/', (req, res) => {
-    var { lat, long } = req.body;
-    if (lat && long) {
-        bot.sendLocation(1771234776, lat, long);
+    try {
+        var { lat, long } = req.body;
+        if (lat && long) {
+            bot.sendLocation(1771234776, lat, long);
+        }
+        res.json(lat, long);
+    } catch (error) {
+        res.json(error.message);
     }
-    res.send('ok');
 });
 
 app.listen(8080, console.log(8080));
